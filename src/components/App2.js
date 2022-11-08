e9b778aa42829e28af9a522674cc51c3d303ccaf
Jose45f <jose.fuentes@jjay.cuny.edu>
2022-11-08 03:55:59 -0500

    finial touches on it.

--- src/components/App2.js
+++ src/components/App2.js
@@ -3,57 +3,90 @@ import './App.css';
 import React, { useState} from 'react';
 
 function App2() {
-const [data, setData] = useState(null);
-const [print, setPrint] = useState(false);
+  const [inputarr,
+    setInputarr] = useState([])
 
-function getData(val) {
-  setData(val.target.value)
-  setPrint(false)
-  console.log(val.target.value) 
+const [inputdata,
+    SetInputdata] = useState({
+      name: "",
+       review: ""})
+
+function changehandle(e) {
+
+    SetInputdata({
+        ...inputdata,
+        [e.target.name]: e.target.value
+    })
+
 }
 
-const handleSubmit = event => {
-  event.preventDefault();
-  alert('You have submitted the form.')
+let {name, review} = inputdata;
+function changhandle() {
+    setInputarr([
+        ...inputarr, {
+            name,
+            review
+        }
+    ])
+
+    console.log(inputdata)
+    SetInputdata({name: "", review: ""})
+
+}
+let delethandle =(i)=>{
+    let newdataArr=[...inputarr]
+    newdataArr.splice(i,1)
+    setInputarr(newdataArr)
 }
 
+return (
+    <div>
+        <input
+            type="text"
+            name='name'
+            value={inputdata.name}
+            onChange={changehandle}
+            placeholder="Enter Name"/>
+        <input
+            type="text"
+            name='review'
+            value={inputdata.review}
+            onChange={changehandle}
+            placeholder="Review"/>
 
-  return (
-    <div className='mov'>
-      <h2> Leave your thoughts below!</h2> 
-      <form onSubmit={handleSubmit}>
-      <textarea required className='bottom' 
-        placeholder="Thoughts on this movie?" onChange={getData}/> 
-        <div className='bottom'>  
-        <button type='submit' onClick={()=> setPrint(true)}>
-        Submit  </button>
-        <div className='left'>
-        <table>
-          <thead>
-            <tr>
-          <th>Reviews:</th> 
-          </tr> 
-          </thead>
-          <tbody>
-            <tr>
-            <td>{ print ?
-    <p className='mov'> {data}</p>
-    :null 
-    }</td>
-          </tr>
-            <tr>
-            <td> Overall good movie!</td>
-          </tr>
-          </tbody>
-          </table>
-          </div>
-      </div>
-</form>
+        <button onClick={changhandle}>Enter
+        </button><br/>
+      <br/><br/>
+
+        <table className='right' border={1} width="30%" cellPadding={10}>
+            <tbody>
+                <tr>
+                    <td>No</td>
+                    <th>Name </th>
+                    <th>Review</th>
+                    <th>Remove</th>
+                </tr>
+                {inputarr.length < 1 ?
+                    <tr>
+                        <td colSpan={4}>No reviews yet, want to change that?</td>
+                    </tr>:
+                inputarr.map((info, ind) => {
+                    return (
+                        <tr key={ind}>
+                            <td>{ind + 1}</td>
+                            <td>{info.name}</td>
+                            <td>{info.review}</td>
+                            <td><button onClick={()=>delethandle(ind)}>Delete</button></td>
+                        </tr>
+                    )
+                })
+}
+
+            </tbody>
+        </table>
     </div>
-
-  );
-};
-
+);
+}
 
 
 
