a7bbd09e5922cf4efc8d9ecfb04dc20a79c54af6
Jose45f <jose.fuentes@jjay.cuny.edu>
2022-11-08 02:44:42 -0500

    one more

--- src/components/App2.js
+++ src/components/App2.js
@@ -1,6 +1,6 @@
 //Rating with box components
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 
 function App2() {
 const [data, setData] = useState(null);
@@ -12,26 +12,47 @@ function getData(val) {
   console.log(val.target.value) 
 }
 
+const handleSubmit = event => {
+  event.preventDefault();
+  alert('You have submitted the form.')
+}
 
 
   return (
     <div className='mov'>
-      <h2> Place holder text. </h2> 
-      <textarea className='bottom' 
-        placeholder="Thoughts on this movie?" onChange={getData}/>
+      <h2> Leave your thoughts below!</h2> 
+      <form onSubmit={handleSubmit}>
+      <textarea required className='bottom' 
+        placeholder="Thoughts on this movie?" onChange={getData}/> 
         <div className='bottom'>  
-     { print ?
-    <p> {data}</p>
+        <button type='submit' onClick={()=> setPrint(true)}>
+        Submit  </button>
+        <div className='left'>
+        <table>
+          <thead>
+            <tr>
+          <th>Reviews:</th> 
+          </tr> 
+          </thead>
+          <tbody>
+            <tr>
+            <td>{ print ?
+    <p className='mov'> {data}</p>
     :null 
-    }
-      <button onClick={()=> setPrint(true)}>
-        Submit  </button> 
-      
+    }</td>
+          </tr>
+            <tr>
+            <td> Overall good movie!</td>
+          </tr>
+          </tbody>
+          </table>
+          </div>
       </div>
+</form>
     </div>
+
   );
 };
-
 
 
 
